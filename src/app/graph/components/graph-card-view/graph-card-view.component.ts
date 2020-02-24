bd9e185cae8dcadbd581cef466f265362e48fa56
David Strong <david.strong@merrillcorp.com>
2020-02-23 18:33:25 -0700

    added card animation

--- src/app/graph/components/graph-card-view/graph-card-view.component.ts
+++ src/app/graph/components/graph-card-view/graph-card-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { Graph } from '../../graph.model';
+import { graphLoaded } from '../../graph.actions';
 
 @Component( {
   selector: 'app-graph-card-view',
@@ -10,4 +11,16 @@ export class GraphCardComponent {
   @Input() graph: Graph[];
   @Input() activeChild: Graph;
   @Output() select = new EventEmitter();
+
+  loaded: boolean = false;
+
+  ngAfterViewInit () {
+    window.setTimeout( () => this.loaded = true, 1000 );
+  }
+
+  selectCard ( child ) {
+    this.loaded = false;
+    window.setTimeout( () => this.loaded = true, 100 );
+    this.select.emit( child );
+  }
 }
